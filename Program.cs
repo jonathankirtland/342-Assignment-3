61871760eca7616d393978dfed42ad8c4673f9cf
Jonathan Kirtland <42749198+jonathankirtland@users.noreply.github.com>
2023-11-14 19:45:57 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-﻿// File: FileReportGenerator.cs
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,81 +6,37 @@ using System.Xml.Linq;
 
 public static class FileReportGenerator
 {
-    // Enumerates all files in a given folder recursively, including subfolders.
     public static IEnumerable<string> EnumerateFilesRecursively(string path)
     {
         if (!Directory.Exists(path))
-        {
             throw new DirectoryNotFoundException($"Directory not found: {path}");
-        }
 
-        return EnumerateFiles(path);
+        return Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
     }
 
-    private static IEnumerable<string> EnumerateFiles(string path)
-    {
-        IEnumerable<string> files = null;
-
-        try
-        {
-            files = Directory.EnumerateFiles(path);
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine($"Error accessing files in: {path}. Error: {ex.Message}");
-            yield break;
-        }
-
-        foreach (string file in files)
-        {
-            yield return file;
-        }
-
-        IEnumerable<string> directories = null;
-        try
-        {
-            directories = Directory.EnumerateDirectories(path);
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine($"Error accessing directories in: {path}. Error: {ex.Message}");
-            yield break;
-        }
-
-        foreach (string directory in directories)
-        {
-            foreach (string file in EnumerateFilesRecursively(directory))
-            {
-                yield return file;
-            }
-        }
-    }
-
-    // Formats a byte size into a human-readable string with two decimal places.
     public static string FormatByteSize(long byteSize)
     {
-        string[] units = { "B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB" };
+        string[] units = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB" };
         double size = byteSize;
         int unitIndex = 0;
 
-        while (size >= 1000 && unitIndex < units.Length - 1)
+        while (size >= 1024 && unitIndex < units.Length - 1)
         {
-            size /= 1000;
+            size /= 1024;
             unitIndex++;
         }
 
         return $"{Math.Round(size, 2)} {units[unitIndex]}";
     }
 
-    // Creates an HTML report from a given collection of file paths.
     public static XDocument CreateReport(IEnumerable<string> files)
     {
         var reportData = files
             .Select(file => new FileInfo(file))
-            .GroupBy(info => info.Extension.ToLowerInvariant())
+            .GroupBy(info => string.IsNullOrEmpty(info.Extension) ? "no extension" : info.Extension.ToLowerInvariant())
             .Select(group => new
             {
-                Type = string.IsNullOrEmpty(group.Key) ? "no extension" : group.Key,
+                Type = group.Key,
                 Count = group.Count(),
                 Size = group.Sum(info => info.Length)
             })
@@ -114,15 +68,12 @@ public static class FileReportGenerator
             return;
         }
 
-        string inputFolderPath = args[0];
-        string outputReportPath = args[1];
-
         try
         {
-            var files = EnumerateFilesRecursively(inputFolderPath);
+            var files = EnumerateFilesRecursively(args[0]);
             XDocument report = CreateReport(files);
-            report.Save(outputReportPath);
-            Console.WriteLine($"Report generated successfully at {outputReportPath}");
+            report.Save(args[1]);
+            Console.WriteLine($"Report generated successfully at {args[1]}");
         }
         catch (Exception ex)
         {
